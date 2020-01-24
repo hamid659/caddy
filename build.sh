97586cc7ef75e354cb280f758a47a87e8eed1173
hamid659 <hamid2160659@gmail.com>
2020-01-23 23:27:09 -0800

    git tag verification added

--- build.sh
+++ build.sh
@@ -2,14 +2,36 @@
 
 #Script to build caddy binary using ubuntu base image
 
+#Function to verify the  git tag
+verify_git_tag () {
+ repo="https://github.com/caddyserver/caddy"
+ if [[ "$#" -ne "1" ]]; then
+   echo "Usage: $0 [version]"
+ fi
+ version=$1
+ git ls-remote --heads --tags $repo | grep -E "refs/(heads|tags)/${version}$" >/dev/null
+ if [[ "$?" -eq "0" ]]; then
+   return 0
+ else
+   echo "Unknown version $version"
+   return 1
+fi
+}
+
+#Verify user input
 if [[ -n "$1" ]]
         then
+           if verify_git_tag $1; then          
             echo  "Building caddy with version $1"
             CADDY_VERSION=$1
+           else
+             exit
+           fi
         else
             echo "Caddy version is not provided. Building with v1.0.4"
             CADDY_VERSION="v1.0.4"
     fi
+
 
 BUILD_ARG="--build-arg CADDY_VERSION=${CADDY_VERSION}"
 IMAGE_NAME="caddy:${CADDY_VERSION}"
